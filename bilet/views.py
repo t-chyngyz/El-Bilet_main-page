cd50221b84312660713f45ca60f450460b5c2e1f
Chyngyz Toichubekov <t.chyngyz@yandex.ru>
2021-10-06 02:13:04 +0600

    Login

--- bilet/views.py
+++ bilet/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 
+@login_required
 def index(request):
     return render(request,'bilet/index.html')
 #
